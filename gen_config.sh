ad64823d26e67bcd42db47e1fe8330eb955e6a5a
Jos Hendriks <jos@hendriks.software>
2018-09-12 20:48:35 +0200

    added nodes

--- gen_config.sh
+++ gen_config.sh
@@ -6,6 +6,12 @@ rpcpassword=$PASSWORD
 rpcallowip=$RPCALLOW
 rpcport=6666
 txindex=1
-addnode=95.179.194.217
-addnode=207.148.106.129
+addnode=95.179.194.217:5545
+addnode=207.148.106.129:5545
+addnode=199.247.31.186:5545
+addnode=149.28.141.186:5545
+addnode=95.179.145.154:35764
+addnode=198.187.28.19:52500
+addnode=5.164.143.162:60070
+addnode=45.64.139.130:56275
 EOF
